cbd56810d1ad9502712a6e03b010d9267ff9cd67
tarunkillscodes <tarunvignesh2005@gmail.com>
2025-09-04 10:39:23 +0530

    final rtl design complete

--- src/peripheral.v
+++ src/peripheral.v
@@ -1,13 +1,3 @@
-/*
- * Copyright (c) 2025 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
-`default_nettype none
-
-// Change the name of this module to something that reflects its functionality and includes your name for uniqueness
-// For example tqvp_yourname_spi for an SPI peripheral.
-// Then edit tt_wrapper.v line 38 and change tqvp_example to your chosen module name.
 module tqvp_example (
     input         clk,          // Clock - the TinyQV project clock is normally set to 64MHz.
     input         rst_n,        // Reset_n - low to reset.
@@ -22,30 +12,128 @@ module tqvp_example (
 
     input         data_write,   // Data write request from the TinyQV core.
     input [7:0]   data_in,      // Data in to the peripheral, valid when data_write is high.
-    
+
     output [7:0]  data_out      // Data out from the peripheral, set this in accordance with the supplied address
 );
 
-    // Example: Implement an 8-bit read/write register at address 0
-    reg [7:0] example_data;
-    always @(posedge clk) begin
-        if (!rst_n) begin
-            example_data <= 0;
+//-----------CSR REGISTERS -------------------//
+    reg [7:0] control_reg;//start,schemesel(1bits),valid_in
+    reg [7:0] status_reg;//ready_out,bitcount(2 bits)
+    reg [7:0] Data_in;
+    reg [7:0] Data_out;
+
+//-----------MAIN FSM -----------------------//
+
+    // States
+    reg [1:0] state, next_state;
+    localparam IDLE   = 2'd0,
+               MAP    = 2'd1,
+               OUTPUT = 2'd2;
+
+    // Next-state logic
+    always @(*) begin
+        next_state = state;
+        case (state)
+            IDLE:   if (status_reg[0]) next_state = MAP;
+            MAP:    next_state = OUTPUT;
+            OUTPUT: if (bit_count == 0) next_state = IDLE;
+                    else next_state = MAP;
+        endcase
+    end
+    
+    // State registers and writing output to the out peripheral
+    always @(posedge clk or negedge rst) begin
+        if (!rst) begin
+            state      <= IDLE;
+            status_reg  <= 8'd0;
         end else begin
-            if (address == 4'h0) begin
-                if (data_write) example_data <= data_in;
+            state <= next_state;
+            if (state == IDLE && valid_in) begin
+                Data_in <= data_in;
+                status_reg[4:2]  <= 3'd8;
+            end else if (state == OUTPUT) begin
+                status_reg[1] <= 1;
+                if (control_reg[1] == 1'b0) begin
+                    Data_in <= Data_in >> 2; // QPSK uses 2 bits
+                    status_reg[4:2]  <= status_reg[4:2] - 2;
+                end else if (control_reg[1] == 1'b1) begin
+                    Data_in <= Data_in >> 4; // 16-QAM uses 4 bits
+                    status_reg[4:2]  <= status_reg[4:2] - 4;
+                end else if(status_reg[4:2] == 3'd0) begin 
+                    status_reg[0] <= 1;
             end
         end
     end
 
-    // All output pins must be assigned. If not used, assign to 0.
-    assign uo_out  = ui_in + example_data;  // Example: uo_out is the sum of ui_in and the example register
+//---------MAPPINNNNN---------------------//
+    // Symbol mapping
+    always @(*) begin
+        Data_out  = 0;
+        status_reg[1] = 0;
+        
+        if (state == MAP) begin
+            if (control_reg[1] == 0) begin
+                // QPSK mapping (Gray code)
+                
+                case (Data_in[1:0])
+                    2'b00: begin Data_out[3:0] =  1; Data_out[7:4] =  1; end
+                    2'b01: begin Data_out[3:0] = -1; Data_out[7:4] =  1; end
+                    2'b11: begin Data_out[3:0] = -1; Data_out[7:4] = -1; end
+                    2'b10: begin Data_out[3:0] =  1; Data_out[7:4] = -1; end
+                    default: begin Data_out[3:0] = 0; Data_out[7:4] = 0; end
+                endcase
+            end else if (control_reg[1] == 1) begin
+                // 16-QAM mapping (Gray code)
+                bit_group = Data_in[3:0];
+                case (Data_in[3:0])
+                    4'b0000: begin Data_out[3:0] =-3; Data_out[7:4]=-3; end
+                    4'b0001: begin Data_out[3:0] =-3; Data_out[7:4]=-1; end
+                    4'b0011: begin Data_out[3:0] =-3; Data_out[7:4]= 1; end
+                    4'b0010: begin Data_out[3:0] =-3; Data_out[7:4]= 3; end
+                    4'b0100: begin Data_out[3:0] =-1; Data_out[7:4]=-3; end
+                    4'b0101: begin Data_out[3:0] =-1; Data_out[7:4]=-1; end
+                    4'b0111: begin Data_out[3:0] =-1; Data_out[7:4]= 1; end
+                    4'b0110: begin Data_out[3:0] =-1; Data_out[7:4]= 3; end
+                    4'b1100: begin Data_out[3:0] = 1; Data_out[7:4]=-3; end
+                    4'b1101: begin Data_out[3:0] = 1; Data_out[7:4]=-1; end
+                    4'b1111: begin Data_out[3:0] = 1; Data_out[7:4]= 1; end
+                    4'b1110: begin Data_out[3:0] = 1; Data_out[7:4]= 3; end
+                    4'b1000: begin Data_out[3:0] = 3; Data_out[7:4]=-3; end
+                    4'b1001: begin Data_out[3:0] = 3; Data_out[7:4]=-1; end
+                    4'b1011: begin Data_out[3:0] = 3; Data_out[7:4]= 1; end
+                    4'b1010: begin Data_out[3:0] = 3; Data_out[7:4]= 3; end
+                    default: begin Data_out[3:0] = 0; Data_out[7:4]= 0; end
+                endcase
+            end
+        end
+    end
+//-------------REGSITER WRITE ------------------------//
 
-    // Address 0 reads the example data register.  
-    // Address 1 reads ui_in
-    // All other addresses read 0.
-    assign data_out = (address == 4'h0) ? example_data :
-                      (address == 4'h1) ? ui_in :
-                      8'h0;    
 
-endmodule
+always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            control_reg <= 8'b0;
+            Data_in     <= 8'b0;
+        end else if (data_write) begin
+            case (address)
+                4'h0: control_reg <= data_in;
+                4'h2: Data_in     <= data_in;
+            endcase
+        end
+    end
+
+
+//--------------REGSISTER READ ------------------//
+
+  always @(*) begin
+        case (address)
+            4'h0: data_out = control_reg;
+            4'h1: data_out = status_reg;
+            4'h2: data_out = Data_in;
+            4'h3: if(status_reg[0] == 1)data_out = Data_out;
+            default: data_out = 8'h00;
+        endcase
+    end
+
+
+
